3aacd5987212a43802108ce98031f7494b8d8cb3
Stephan.Mahieu <Stephan.Mahieu@2f336c3e-4775-41a3-c73e-39e6fd5299aa>
2011-01-03 11:56:52 +0000

    Support for new html5 (text) input fields

--- trunk/chrome/content/overlay/FhcContextMenu.js
+++ trunk/chrome/content/overlay/FhcContextMenu.js
@@ -144,7 +144,7 @@ const FhcContextMenu = {
 
       case "contentAreaContextMenu":
         var inputField = document.commandDispatcher.focusedElement;
-        var isInputText = (inputField != null && "INPUT" == inputField.nodeName && "text" == inputField.type);
+        var isInputText = FhcUtil.isInputTextElement(inputField);
         var isValueInFormHistory = isInputText && this._isValueInFormHistory(inputField);
         hasFields = this._containsInputFields();
         manualSaveDisabled = !this.preferences.isManualsaveEnabled();
@@ -206,7 +206,7 @@ const FhcContextMenu = {
    */
   menuDeleteValueThisField: function() {
     var inputField = document.commandDispatcher.focusedElement;
-    var isInputText = (inputField != null && "INPUT" == inputField.nodeName && "text" == inputField.type);
+    var isInputText = FhcUtil.isInputTextElement(inputField);
     var isValueInFormHistory = isInputText && this._isValueInFormHistory(inputField);
     if (!isValueInFormHistory) {
       return;
@@ -245,7 +245,7 @@ const FhcContextMenu = {
    */
   menuDeleteEntriesThisField: function() {
     var inputField = document.commandDispatcher.focusedElement;
-    var isInputText = (inputField != null && "INPUT" == inputField.nodeName && "text" == inputField.type);
+    var isInputText = FhcUtil.isInputTextElement(inputField);
     if (!isInputText) {
       return;
     }
