7521d793046752a35da5db46bd953ea47b939c1b
Stephan.Mahieu <Stephan.Mahieu@2f336c3e-4775-41a3-c73e-39e6fd5299aa>
2011-05-05 06:43:45 +0000

    Use addEventListener instead of setAttribute (Mozilla validation warning)

---
+++ tags/Release_1.2.9.1/build_postprocess_EN-NL.cmd
@@ -0,0 +1,56 @@
+@echo off
+
+set XPI_FILE=formhistory_1291.xpi
+
+
+rem --(check if xpi exist)-------------------------------------
+if exist %XPI_FILE% goto continue
+echo.
+echo -------------------------------------------
+echo XPI file not found (%XPI_FILE%)!!!
+echo -------------------------------------------
+echo.
+goto error
+:continue
+
+rem --(delete incomplete locales)-------------------------------
+7z.exe d %XPI_FILE% ^
+          chrome\locale\bg-BG ^
+          chrome\locale\cs ^
+          chrome\locale\da ^
+          chrome\locale\de ^
+          chrome\locale\es-ES ^
+          chrome\locale\fr ^
+          chrome\locale\hu ^
+          chrome\locale\it-IT ^
+          chrome\locale\pt-BR ^
+          chrome\locale\ru ^
+          chrome\locale\sk-SK ^
+          chrome\locale\sv-SE ^
+          chrome\locale\zh-CN
+if errorlevel 1 goto error
+
+rem --(delete all amo.properties)-------------------------------
+7z.exe d -r %XPI_FILE% amo.properties
+if errorlevel 1 goto error
+
+rem --(delete leftover files)-----------------------------------
+7z.exe d -r %XPI_FILE% *.7z
+if errorlevel 1 goto error
+
+goto okay
+:error
+echo.
+echo ===============
+echo Error detected!
+echo ===============
+pause
+goto end
+
+:okay
+echo.
+echo --------
+echo Finished
+echo --------
+echo.
+:end
