e139a127cb1430b7905b1b0a5e9f0084388c1775
Stephan.Mahieu <Stephan.Mahieu@2f336c3e-4775-41a3-c73e-39e6fd5299aa>
2011-07-09 23:11:15 +0000

    Added multiline save (WIP) :: default save after 500 char length change (old: 100)

--- trunk/defaults/preferences/FhcPreferences.js
+++ trunk/defaults/preferences/FhcPreferences.js
@@ -46,7 +46,7 @@ pref("extensions.formhistory.backgroundTree", "auto");
 pref("extensions.formhistory.customTreeSkin", "CustomDefault");
 pref("extensions.formhistory.multiline.backupenabled", true);
 pref("extensions.formhistory.multiline.saveolder", "10");
-pref("extensions.formhistory.multiline.savelength", "100");
+pref("extensions.formhistory.multiline.savelength", "500");
 pref("extensions.formhistory.multiline.deleteolder", "10080");
 pref("extensions.formhistory.multiline.exception", "multilinenoexception");
 pref("extensions.formhistory.multiline.savealways", false);
