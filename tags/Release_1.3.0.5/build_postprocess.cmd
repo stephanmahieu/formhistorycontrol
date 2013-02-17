f52643c5be271eb59bf89c2919cc77162af4dde6
Stephan.Mahieu <Stephan.Mahieu@2f336c3e-4775-41a3-c73e-39e6fd5299aa>
2013-02-17 12:39:43 +0000

    Tag release 1.3.0.5

---
+++ tags/Release_1.3.0.5/build_postprocess.cmd
@@ -0,0 +1,50 @@
+@echo off
+
+set XPI_FILE=formhistory_1305.xpi
+
+
+rem --(check if xpi exist)-------------------------------------
+if exist %XPI_FILE% goto continue
+echo.
+echo -------------------------------------------
+echo XPI file not found (%XPI_FILE%)!!!
+echo -------------------------------------------
+echo.
+goto error
+:continue
+
+rem --(delete incomplete locales)-------------------------------
+7z.exe d %XPI_FILE% ^
+          chrome\locale\bg-BG ^
+          chrome\locale\hu ^
+          chrome\locale\it ^
+          chrome\locale\ru ^
+          chrome\locale\sk-SK ^
+          chrome\locale\sv-SE ^
+          chrome\locale\zh-CN
+if errorlevel 1 goto error
+
+rem --(delete all amo.properties)-------------------------------
+7z.exe d -r %XPI_FILE% amo.properties
+if errorlevel 1 goto error
+
+rem --(delete leftover files)-----------------------------------
+7z.exe d -r %XPI_FILE% *.7z
+if errorlevel 1 goto error
+
+goto okay
+:error
+echo.
+echo ===============
+echo Error detected!
+echo ===============
+pause
+goto end
+
+:okay
+echo.
+echo --------
+echo Finished
+echo --------
+echo.
+:end
