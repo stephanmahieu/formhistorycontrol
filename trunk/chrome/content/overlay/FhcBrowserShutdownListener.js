2485e8b35d459f37855fdc64e71893a33ec18096
Stephan.Mahieu <Stephan.Mahieu@2f336c3e-4775-41a3-c73e-39e6fd5299aa>
2014-12-14 18:06:05 +0000

    Cleanup at shutdown of formelements database (radiobuttons, checkboxes, selects)

--- trunk/chrome/content/overlay/FhcBrowserShutdownListener.js
+++ trunk/chrome/content/overlay/FhcBrowserShutdownListener.js
@@ -86,6 +86,7 @@ const FhcBrowserShutdownListener = {
         if (this.prefHandler.isCleanupOnShutdown()) {
           this.cleanupFormHistory();
         }
+        this.cleanupFormElementsHistory();
         window.removeEventListener("unload", FhcBrowserShutdownListener, false);
         this.destroy();
         break;
@@ -120,6 +121,20 @@ const FhcBrowserShutdownListener = {
     allEntries = null;
   },
 
+  /**
+   * Cleanup the formelements database.
+   */
+  cleanupFormElementsHistory: function() {
+    // re-use the expiration preference for textfields (default 180 days)
+    var expireDays = this.prefHandler.getGlobalFormfillExpireDays();
+
+    var d = new Date();
+    var now = d.getTime() * 1000;
+    var treshold = now - (expireDays * 24 * 60 * 60 * 1000 * 1000);
+    
+    this.dbHandler.deleteFormElementsOlder(treshold);
+  },
+  
   /**
    * Send notification to observers that the formhistory store has changed.
    */
